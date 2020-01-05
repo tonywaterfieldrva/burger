7a97c4c90370a0f510f2c3b5d47aa248b23e70ac
Tony Waterfield <tonywaterfieldrva@gmail.com>
2020-01-05 10:32:07 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 var app = express();
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
