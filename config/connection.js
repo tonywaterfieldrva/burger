d92c1e83558caefa3c6d923b496fe53be4fc2203
Tony Waterfield <tonywaterfieldrva@gmail.com>
2020-01-16 23:40:25 -0500

    updating mysql connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Reload89!",
-    database: "burgers_db"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    user: "bbd3f48bfef5b6",
+    password: "43bee7a8",
+    database: "heroku_c5e08eff384d466"
   });
 }
 
