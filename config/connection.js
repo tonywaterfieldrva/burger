25bd3b4a2f86d4215da16b2d27ec53b45994d8be
Tony Waterfield <tonywaterfieldrva@gmail.com>
2020-01-16 21:04:31 -0500

    update Jaws

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,34 @@
-var mysql = require("mysql");
 
-connection = mysql.createConnection({
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Reload89!",
     database: "burgers_db"
-});
+  });
+}
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id: " + connection.threadId);
-    
-});
-
+connection.connect();
 module.exports = connection;
+// var mysql = require("mysql");
+
+// connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "Reload89!",
+//     database: "burgers_db"
+// });
+
+
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id: " + connection.threadId);
+// });
+
+// module.exports = connection;
